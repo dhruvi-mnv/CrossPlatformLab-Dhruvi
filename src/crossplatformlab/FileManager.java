2a0dab5f5403604db8d9d3c7c494bff8d4a896fc
dhruvi-mnv <jariwadh@sheridancollege.ca>
2026-02-19 17:31:29 -0500

    Midterm PartB Q-2 1st Commit by Dhruvi

---
+++ src/crossplatformlab/FileManager.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package crossplatformlab;
+
+/**
+ *
+ * @author dhruv
+ */
+public class FileManager {
+    
+    //adding some code
+    public String greet() {
+        return "Hello World!!";
+    }
+    
+}
